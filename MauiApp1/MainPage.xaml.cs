84c45a899f30701ea6028e5e8d90fc70a02305a3
Laurent0506 <laurent.cea@live.com>
2022-11-06 17:16:24 +0100

    Bass

---
+++ MauiApp1/MainPage.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System.Runtime.InteropServices;
+using Un4seen.Bass;
+
+namespace MauiApp1;
+
+public partial class MainPage : ContentPage
+{
+
+    public MainPage()
+	{
+		InitializeComponent();
+ 
+    }
+
+    private async void OnBassClicked(object sender, EventArgs e)
+    {
+        int bassVersion;
+        nint libHandle;
+
+        if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+            await DisplayAlert("Info", "On Windows", "OK");
+        else
+        {
+            await DisplayAlert("Info", "not Windows platform ... test stoped", "OK");
+            return;
+        }
+            
+        if (NativeLibrary.TryLoad("Bass.Net.dll", out libHandle))
+            await DisplayAlert("Info", "Bass.Net.dll loaded", "OK"); 
+        else
+            await DisplayAlert("Info", "Bass.Net.dll not loaded", "OK");
+
+        if (NativeLibrary.TryLoad("bass.dll", out libHandle))
+            await DisplayAlert("Info", "bass.dll loaded", "OK");
+        else
+            await DisplayAlert("Info", "bass.dll not loaded", "OK");
+
+        if (NativeLibrary.TryLoad("bass_fx.dll", out libHandle))
+            await DisplayAlert("Info", "bass_fx.dll loaded", "OK");
+        else
+            await DisplayAlert("Info", "bass_fx.dll not loaded", "OK");
+
+        bassVersion = Bass.BASS_GetVersion();
+
+        await DisplayAlert("Info", bassVersion.ToString(), "OK");  
+    }
+
+}
+
